87eae2d084bfae708f393f8051f86c1004ef28d8
Matt Crouch <matt@mattcrouch.net>
2020-01-03 17:54:47 +0000

    Layout with Grid

--- src/App/index.js
+++ src/App/index.js
@@ -4,11 +4,16 @@ import "./styles.css";
 
 const App = () => (
   <div className="app">
-    <div className="app__section">
-      <Heading>Uptime (last 24 hours) Uptime (last 24 hours)</Heading>
-    </div>
-    <div className="app__section">
-      <Heading>Response times</Heading>
+    <div className="app__sections">
+      <section className="app__uptime">
+        <Heading>Uptime (last 24 hours) Uptime (last 24 hours)</Heading>
+      </section>
+      <section className="app__response">
+        <Heading>Response times</Heading>
+      </section>
+      <section className="app__graph">
+        <Heading>Last half hour</Heading>
+      </section>
     </div>
   </div>
 );
