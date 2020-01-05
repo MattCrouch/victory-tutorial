d0a63aad0ff51111d4c7d96b15b7148870dcf3cf
Matt Crouch <matt@mattcrouch.net>
2020-01-05 14:41:18 +0000

    Slight resize

--- src/App/index.js
+++ src/App/index.js
@@ -7,7 +7,7 @@ const App = () => (
   <div className="app">
     <div className="app__sections">
       <section className="app__uptime">
-        <Heading>Uptime (last 24 hours)</Heading>
+        <Heading>Uptime</Heading>
         <Uptime value={0.8} />
       </section>
       <section className="app__response">
--- src/Uptime/index.js
+++ src/Uptime/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { VictoryPie } from "victory";
+import { VictoryLabel, VictoryPie } from "victory";
 import "./styles.css";
 
 const Uptime = ({ value }) => {
@@ -9,25 +9,49 @@ const Uptime = ({ value }) => {
 
   return (
     <div className="uptime">
-      <VictoryPie
-        animate={{ duration: 500 }}
-        data={[
-          { x: "uptime", y: percentage },
-          { x: "downtime", y: 1 - percentage }
-        ]}
-        innerRadius={150}
-        labels={() => null}
-        padding={0}
-        style={{
-          data: {
-            fill: ({ datum }) => {
-              const color =
-                datum.y > 0.9 ? "var(--success-color)" : "var(--warning-color)";
-              return datum.x === "uptime" ? color : "transparent";
+      {/* <svg> required when `standalone` is false
+      `standalone` required to place label on top of pie chart */}
+      <svg
+        width={400}
+        height={400}
+        role="img"
+        viewBox="0 0 400 400"
+        style={{ width: "100%", height: "auto" }}
+      >
+        <VictoryPie
+          animate={{ duration: 200 }}
+          data={[
+            { x: "uptime", y: percentage },
+            { x: "downtime", y: 1 - percentage }
+          ]}
+          innerRadius={150}
+          labels={() => null}
+          padding={0}
+          standalone={false}
+          style={{
+            data: {
+              fill: ({ datum }) => {
+                const color =
+                  datum.y > 0.9
+                    ? "var(--success-color)"
+                    : "var(--warning-color)";
+                return datum.x === "uptime" ? color : "transparent";
+              }
             }
-          }
-        }}
-      />
+          }}
+        />
+        <VictoryLabel
+          textAnchor="middle"
+          verticalAnchor="middle"
+          x={200}
+          y={200}
+          style={{
+            fontFamily: "Roboto Mono, Roboto, sans-serif",
+            fontSize: "64px"
+          }}
+          text={`${Math.round(percentage * 100)}%`}
+        />
+      </svg>
     </div>
   );
 };
