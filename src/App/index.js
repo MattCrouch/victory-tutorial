43b8fa2b47d45dac723cdcacd848ceb92c70154c
Matt Crouch <matt@mattcrouch.net>
2020-01-08 22:20:59 +0000

    Switch between endpoints

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,17 @@
 import React, { useContext, useState } from "react";
+import EndpointButton from "../EndpointButton";
 import Heading from "../Heading";
 import Uptime from "../Uptime";
 import ResponseChart from "../ResponseChart";
 import { ResponseTimesContext } from "../DataProvider";
+import ENDPOINTS from "../Endpoints";
 import "./styles.css";
 
 const App = () => {
   const responseTimes = useContext(ResponseTimesContext);
-  const [currentEndpoint] = useState(Object.keys(responseTimes)[0]);
+  const [currentEndpointKey, setCurrentEndpointKey] = useState(
+    Object.keys(responseTimes)[0]
+  );
 
   return (
     <div className="app">
@@ -16,12 +20,29 @@ const App = () => {
           <Heading>Uptime</Heading>
           <Uptime />
         </section>
-        <section className="app__response">
-          <Heading>Response times</Heading>
+        <section className="app__endpoints">
+          <Heading>Endpoints</Heading>
+          <ul className="app__endpoints-list">
+            {Object.keys(responseTimes).map(endpointId => {
+              const endpointData = ENDPOINTS.find(
+                endpoint => endpoint.id === endpointId
+              );
+
+              return (
+                <li className="app__endpoints-list-item" key={endpointId}>
+                  <EndpointButton
+                    endpoint={endpointData}
+                    responseTime={responseTimes[endpointId].slice(-1)[0].length}
+                    onClick={() => setCurrentEndpointKey(endpointId)}
+                  />
+                </li>
+              );
+            })}
+          </ul>
         </section>
-        <section className="app__graph">
-          <Heading>Last hour</Heading>
-          <ResponseChart currentEndpoint={currentEndpoint} />
+        <section className="app__response">
+          <Heading>Response time</Heading>
+          <ResponseChart currentEndpoint={currentEndpointKey} />
         </section>
       </div>
     </div>
---
+++ src/EndpointButton/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
+
+const EndpointButton = ({ endpoint, onClick, responseTime }) => (
+  <button className="endpointButton" onClick={onClick}>
+    <div className="endpointButton__container">
+      <div className="endpointButton__endpoint">
+        <span className="endpointButton__method">{endpoint.method}</span>
+        <span className="endpointButton__route">{endpoint.route}</span>
+      </div>
+      <div className="endpointButton__response-time">
+        {responseTime.toFixed(2)}
+      </div>
+    </div>
+  </button>
+);
+
+EndpointButton.propTypes = {
+  endpoint: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    method: PropTypes.string.isRequired,
+    route: PropTypes.string.isRequired
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
+  responseTime: PropTypes.number.isRequired
+};
+
+export default EndpointButton;
